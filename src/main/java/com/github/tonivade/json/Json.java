cc4a03db015e80e2ee295490836ba520ac66760a
Antonio Muñoz <antoniogmc@gmail.com>
2020-10-21 18:20:13 +0200

    minor changes

--- src/main/java/com/github/tonivade/json/Json.java
+++ src/main/java/com/github/tonivade/json/Json.java
@@ -20,10 +20,8 @@ import java.lang.reflect.Type;
 import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
 import org.petitparser.context.Result;
